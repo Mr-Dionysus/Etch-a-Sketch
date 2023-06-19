a14d5ee6625f949e8e61eb5b3ea92df4b53d4b29
Denis <dbreznikov@gmail.com>
2023-06-19 10:03:01 +0300

    Create button that change num of squares and add random RGB trail

--- app.js
+++ app.js
@@ -5,10 +5,48 @@ for (let i = 0; i < 256; i++) {
     mainBox.appendChild(gridDiv);
 }
 
-const gridBlocks = document.querySelectorAll("#mainBox div");
+let gridBlocks = document.querySelectorAll("#mainBox div");
+
+function randomRGB() {
+    const firstColor = Math.floor(Math.random() * 255);
+    const secondColor = Math.floor(Math.random() * 255);
+    const thirdColor = Math.floor(Math.random() * 255);
+    return `rgb(${firstColor},${secondColor},${thirdColor})`;
+}
 
 gridBlocks.forEach((gridBlock) => {
     gridBlock.addEventListener("mouseover", () => {
-        gridBlock.style.backgroundColor = "black";
+        gridBlock.style.backgroundColor = randomRGB();
+    });
+});
+
+const createNewGrid = document.querySelector("button");
+
+createNewGrid.addEventListener("click", () => {
+    mainBox.replaceChildren();
+
+    let newGrid = 101;
+    while (newGrid > 100) {
+        newGrid = +prompt(
+            "Write how many squares per side do you want but <= 100"
+        );
+    }
+
+    if (newGrid > 100) {
+        return false;
+    }
+
+    for (let i = 0; i < newGrid ** 2; i++) {
+        let gridDiv = document.createElement("div");
+        gridDiv.style.width = `${mainBox.offsetWidth / newGrid}px`;
+        gridDiv.style.height = gridDiv.style.width;
+        mainBox.appendChild(gridDiv);
+    }
+
+    let gridBlocks = document.querySelectorAll("#mainBox div");
+    gridBlocks.forEach((gridBlock) => {
+        gridBlock.addEventListener("mouseover", () => {
+            gridBlock.style.backgroundColor = randomRGB();
+        });
     });
 });

978e592547a6c091b20fc4fed7731af3a3e83448
Denis <dbreznikov@gmail.com>
2023-06-19 07:58:37 +0300

    Create hover effect to draw trail of pixels on grid

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ for (let i = 0; i < 256; i++) {
     let gridDiv = document.createElement("div");
     mainBox.appendChild(gridDiv);
 }
+
+const gridBlocks = document.querySelectorAll("#mainBox div");
+
+gridBlocks.forEach((gridBlock) => {
+    gridBlock.addEventListener("mouseover", () => {
+        gridBlock.style.backgroundColor = "black";
+    });
+});
